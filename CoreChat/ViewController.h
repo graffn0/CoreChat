991bd5984e6065ad3f82f4ad52dfb3feec34483a
admin <admin@admins-macbook-pro.local>
2016-04-19 09:19:15 -0400

    Initial Commit

---
+++ CoreChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreChat
+//
+//  Created by admin on 4/19/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
